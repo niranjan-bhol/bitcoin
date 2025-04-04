1bd9b3b1ba741c8302bc2454fb31978b0825b294
Niranjan Bhol <niranjan.bhol@icloud.com>
2025-04-05 03:21:23 +0530

    solved decimal mismatch issue | from 0.0021 A to 21000000 A

--- contracts/AlphaToken.sol
+++ contracts/AlphaToken.sol
@@ -5,6 +5,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract AlphaToken is ERC20 {
     constructor() ERC20("Alpha", "A") {
-        _mint(msg.sender, 21_000_000 * 10 ** 8); // 21 million tokens with 8 decimals
+        _mint(msg.sender, 21_000_000 * 10 ** 8); // 21 million with 8 decimals
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 8;
     }
 }
